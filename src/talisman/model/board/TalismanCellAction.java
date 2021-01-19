30264ef070fe710567d024a8f24f253921f9bb70
Alb-Ard <alberto.arduini3@studio.unibo.it>
2021-01-19 15:33:57 +0100

    Aggiunta l'interfaccia per le azioni delle celle e l'enumerazione per il tipo di ambientazione della cella con Javadoc.

---
+++ src/talisman/model/board/TalismanCellAction.java
@@ -0,0 +1,23 @@
+package talisman.model.board;
+
+import java.io.Serializable;
+
+/**
+ * Interface for actions that will be applied to a player
+ * when it reaches a cell
+ * 
+ * @author Alberto Arduini
+ *
+ */
+public interface TalismanCellAction extends Serializable {
+	/**
+	 * Obtains the action's description
+	 * @return the description
+	 */
+	String getDescription();
+	/**
+	 * Applies the action to the specified player
+	 * @param player the player on which the action will execute
+	 */
+	void applyTo(int player);
+}
---
+++ src/talisman/model/board/TalismanCellType.java
@@ -0,0 +1,5 @@
+package talisman.model.board;
+
+public enum TalismanCellType {
+	MONSTER, BIOME, ZONE,
+}
