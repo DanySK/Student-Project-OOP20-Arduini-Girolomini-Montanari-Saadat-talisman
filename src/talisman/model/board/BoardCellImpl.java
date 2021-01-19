609453e8748b021a5dab882f94f3c640bb3f5870
Alb-Ard <alberto.arduini3@studio.unibo.it>
2021-01-19 15:24:16 +0100

    Aggiunta implementazione base della cella con Javadoc.

---
+++ src/talisman/model/board/BoardCellImpl.java
@@ -0,0 +1,37 @@
+package talisman.model.board;
+
+/**
+ * Basic implementation of a board cell
+ * 
+ * @author Alberto Arduini
+ *
+ */
+public class BoardCellImpl implements BoardCell {
+	private final String imagePath;
+	private final String text;
+	/**
+	 * Creates a new cell
+	 * @param imagePath the path to the cell image/icon
+	 * @param text the text to show on the cell
+	 */
+	public BoardCellImpl(String imagePath, String text) {
+		this.imagePath = imagePath;
+		this.text = text;
+	}
+	/**
+	 * Gets the path to the cell image
+	 * @return the image's path
+	 */
+	@Override
+	public String getImagePath() {
+		return this.imagePath;
+	}
+	/**
+	 * Gets the cell text or description
+	 * @return the cell's text
+	 */
+	@Override
+	public String getText() {
+		return this.text;
+	}
+}
