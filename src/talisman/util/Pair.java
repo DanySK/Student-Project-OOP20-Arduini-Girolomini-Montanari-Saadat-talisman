605549b40284b3f4d19d1dede1c659e205286f46
Alb-Ard <alberto.arduini3@studio.unibo.it>
2021-01-28 18:06:49 +0100

    Creata classe Pair per le tuple di dati

---
+++ src/talisman/util/Pair.java
@@ -0,0 +1,16 @@
+package talisman.util;
+
+public class Pair<X, Y> {
+	private final X x;
+	private final Y y;
+	public Pair(final X x, final Y y) {
+		this.x = x;
+		this.y = y;
+	}
+	public X getX() {
+		return x;
+	}
+	public Y getY() {
+		return y;
+	}
+}
