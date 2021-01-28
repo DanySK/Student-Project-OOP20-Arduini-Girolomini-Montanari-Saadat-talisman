96f0d69b1fe52e992310180241fcec8d4062cc68
Alb-Ard <alberto.arduini3@studio.unibo.it>
2021-01-28 18:08:18 +0100

    Terminata implementazione tabellone talisman

--- src/talisman/model/board/TalismanBoard.java
+++ src/talisman/model/board/TalismanBoard.java
@@ -1,35 +1,57 @@
 package talisman.model.board;
 
+import java.util.*;
+import java.util.stream.*;
+
+import talisman.util.*;
+
 /**
  * Implements a board for talisman
  * 
  * @author Alberto Arduini
  */
 public class TalismanBoard implements PopulatedBoard<TalismanBoardSection, TalismanBoardCell, TalismanBoardPawn> {
-
-	@Override
-	public TalismanBoardCell getCell(int section, int index) {
-		// TODO Auto-generated method stub
-		return null;
+	private final List<TalismanBoardSection> sections;
+	private final List<TalismanBoardPawn> characterPawns;
+	private final Map<TalismanBoardPawn, Pair<Integer, Integer>> pawnPositions;
+	/**
+	 * Creates a new talisman board
+	 * @param sections the sections that the board contains
+	 * @param characterPawns the player pawns
+	 */
+	public TalismanBoard(final List<TalismanBoardSection> sections, final  List<TalismanBoardPawn> characterPawns) {
+		super();
+		this.sections = sections;
+		this.characterPawns = characterPawns;
+		this.pawnPositions = this.characterPawns.stream().collect(Collectors.toMap(p -> p, p -> new Pair<>(0, 0)));
 	}
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public TalismanBoardSection getSection(int index) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.sections.get(index);
 	}
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void movePawnTo(int playerIndex, int cell) {
-		// TODO Auto-generated method stub
-		
+		Pair<Integer, Integer> oldPosition = this.pawnPositions.get(this.getPawn(playerIndex));
+		this.pawnPositions.replace(this.getPawn(playerIndex), new Pair<>(oldPosition.getX(), cell));
 	}
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void changePawnSection(int playerIndex, int section, int cell) {
-		// TODO Auto-generated method stub
-		
+		this.pawnPositions.replace(this.getPawn(playerIndex), new Pair<>(section, cell));
 	}
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public TalismanBoardPawn getPawn(int playerIndex) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.characterPawns.get(playerIndex);
 	}
 }
