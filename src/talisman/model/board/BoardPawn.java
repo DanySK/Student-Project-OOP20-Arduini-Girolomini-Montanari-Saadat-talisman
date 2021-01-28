13d43213004bf9c011fccba934a70c8d30d495a5
Alb-Ard <alberto.arduini3@studio.unibo.it>
2021-01-28 17:08:56 +0100

    Create interfaccia e implementazione pedine personaggi

---
+++ src/talisman/model/board/BoardPawn.java
@@ -0,0 +1,15 @@
+package talisman.model.board;
+
+/**
+ * Interface that models a pawn on a board
+ * 
+ * @author Alberto Arduini
+ *
+ */
+public interface BoardPawn {
+	/**
+	 * Gets the path to the pawn's image
+	 * @return the pawn's image
+	 */
+	String getImagePath();
+}
