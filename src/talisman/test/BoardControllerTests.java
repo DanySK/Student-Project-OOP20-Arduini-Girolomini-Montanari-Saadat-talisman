2ba9d0a86cd889f9d755e0fe85a64b3ba8d010cf
Alb-Ard <alberto.arduini3@studio.unibo.it>
2021-02-01 16:29:13 +0100

    Generalizzati controlli per test controller tabellone

--- src/talisman/test/BoardControllerTests.java
+++ src/talisman/test/BoardControllerTests.java
@@ -22,17 +22,30 @@ public class BoardControllerTests {
         final TalismanBoardController controller = BoardTestUtils.createController(2, 4, 1);
         // Move to cell of index two
         controller.moveCharacterCell(0, 2);
-        Assertions.assertEquals(2, controller.getBoard().getPawnCellIndex(0));
+        // The player is now in cell 2, section 0
+        this.assertCellAndSection(controller, 0, 2, 0);
         // Move to cell if index one
         controller.moveCharacterCell(0, 1);
-        Assertions.assertEquals(1, controller.getBoard().getPawnCellIndex(0));
+        // The player is now in cell 1, section 0
+        this.assertCellAndSection(controller, 0, 1, 0);
         // Move to first cell of section of index 1
         controller.moveCharacterSection(0, 1);
-        Assertions.assertEquals(0, controller.getBoard().getPawnCellIndex(0));
-        Assertions.assertEquals(1, controller.getBoard().getPawnSectionIndex(0));
+        // The player is now in cell 0, section 1
+        this.assertCellAndSection(controller, 0, 0, 1);
         // Move to cell of index 3 of section of index 0
         controller.moveCharacterSection(0, 0, 3);
-        Assertions.assertEquals(3, controller.getBoard().getPawnCellIndex(0));
-        Assertions.assertEquals(0, controller.getBoard().getPawnSectionIndex(0));
+        // The player is now in cell 3, section 0
+        this.assertCellAndSection(controller, 0, 3, 0);
+    }
+
+    private void assertCellAndSection(final TalismanBoardController controller, final int player, final int cell, final int section) {
+        // Check cell index
+        Assertions.assertEquals(cell, controller.getBoard().getPawnCellIndex(player));
+        // Check cell instance
+        Assertions.assertEquals(controller.getBoard().getCell(section, cell), controller.getCharacterCell(player));
+        // Check section index
+        Assertions.assertEquals(section, controller.getBoard().getPawnSectionIndex(player));
+        // Check section instance
+        Assertions.assertEquals(controller.getBoard().getSection(section), controller.getCharacterSection(player));
     }
 }
