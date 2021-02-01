8240be1d5046aa90857cbe8fe705a73188344c93
Alb-Ard <alberto.arduini3@studio.unibo.it>
2021-02-01 16:12:13 +0100

    Adattate classi test tabellone con la nuova classe utility

--- src/talisman/test/BoardModelTests.java
+++ src/talisman/test/BoardModelTests.java
@@ -1,19 +1,11 @@
 package talisman.test;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import talisman.model.board.TalismanBoardImpl;
+import talisman.test.util.BoardTestUtils;
+
 import talisman.model.board.TalismanBoard;
-import talisman.model.board.TalismanBoardCell;
-import talisman.model.board.TalismanBoardPawn;
-import talisman.model.board.TalismanBoardSection;
-import talisman.model.board.TalismanBoardSectionImpl;
-import talisman.model.board.TalismanCellType;
 
 /**
  * Tests the talisman board's model.
@@ -27,7 +19,7 @@ public class BoardModelTests {
      */
     @Test
     public void testMovePawn() {
-        final TalismanBoard board = this.createBoard(2, 4, 1);
+        final TalismanBoard board = BoardTestUtils.createBoard(2, 4, 1);
         // Move to cell of index two
         board.movePawnTo(0, 2);
         Assertions.assertEquals(2, board.getPawnCellIndex(0));
@@ -42,29 +34,5 @@ public class BoardModelTests {
         board.changePawnSection(0, 0, 3);
         Assertions.assertEquals(3, board.getPawnCellIndex(0));
         Assertions.assertEquals(0, board.getPawnSectionIndex(0));
-    }
-
-    /**
-     * Creates a test board.
-     * 
-     * @param sectionsCount the number of sections
-     * @param cellsCount    the number of cells of every section
-     * @param pawnsCount    the number of pawns
-     * @return the created board
-     */
-    private TalismanBoard createBoard(final int sectionsCount, final int cellsCount, final int pawnsCount) {
-        final List<TalismanBoardCell> cells = new ArrayList<>();
-        for (int i = 0; i < cellsCount; i++) {
-            cells.add(new TalismanBoardCell("", "Cell " + i, TalismanCellType.BIOME, Set.of()));
-        }
-        final List<TalismanBoardSection> sections = new ArrayList<>();
-        for (int i = 0; i < sectionsCount; i++) {
-            sections.add(new TalismanBoardSectionImpl(List.copyOf(cells)));
-        }
-        final List<TalismanBoardPawn> pawns = new ArrayList<>();
-        for (int i = 0; i < pawnsCount; i++) {
-            pawns.add(new TalismanBoardPawn(""));
-        }
-        return new TalismanBoardImpl(sections, pawns);
     }
 }
