53af8359f464f185051f5c5c7c00d791d2fd9c3d
Alb-Ard <alberto.arduini3@studio.unibo.it>
2021-02-02 16:40:29 +0100

    Implementati i metodi per CellType e sistemata visualizzazione componenti figli nella view della cella

--- src/talisman/view/board/BoardCellViewImpl.java
+++ src/talisman/view/board/BoardCellViewImpl.java
@@ -1,17 +1,19 @@
 package talisman.view.board;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
-
+import java.awt.LayoutManager;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
-
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import talisman.util.CellType;
 import talisman.util.ViewUtils;
 
 /**
@@ -21,10 +23,11 @@ import talisman.util.ViewUtils;
  *
  */
 // getX and getY do not get overridden since they are already define in JComponent
-public class BoardCellViewImpl extends JPanel implements BoardCellView {
+public final class BoardCellViewImpl extends JPanel implements BoardCellView {
     private final JLabel textLabel;
     private final JPanel textBackground;
     private final Image backgroundImage;
+    private final CellType type;
 
     /**
      * Creates a new cell.
@@ -34,36 +37,49 @@ public class BoardCellViewImpl extends JPanel implements BoardCellView {
      * @param type      the type of the cell
      */
     public BoardCellViewImpl(final String imagePath, final String text, final CellType type) {
+        this.type = type;
         // I try to load the specified image
         File imageFile = new File(imagePath);
         // If the image doesn't exist, then I default to the "image not found" image
-        if (!imageFile.exists()) {
-            imageFile = new File(ViewUtils.NO_IMAGE_PATH);
+        if (!imageFile.exists() || !imageFile.isFile()) {
+            imageFile = new File(ViewUtils.PROJ_NO_IMAGE_PATH);
         }
         Image loadedImage = null;
         try {
             loadedImage = ImageIO.read(imageFile);
         } catch (IOException e) {
-            // Shouldn't happen, since the file in "NO_IMAGE_PATH" should always exist
+            // Shouldn't happen, since the file in "*_NO_IMAGE_PATH" should always exist
             e.printStackTrace();
         }
+        final LayoutManager layout = new BorderLayout();
+        this.setLayout(layout);
         this.backgroundImage = loadedImage;
         this.textBackground = new JPanel();
         this.textBackground.setBackground(Color.BLACK);
-        this.add(this.textBackground);
+        this.add(this.textBackground, BorderLayout.PAGE_END);
         this.textLabel = new JLabel();
         this.textLabel.setText(text);
         this.textLabel.setForeground(Color.WHITE);
-        this.add(this.textLabel);
-        // TODO: Size cell to fit type
+        this.textBackground.add(this.textLabel);
+        this.setMinimumSize(new Dimension(100, 100));
     }
 
     /**
      * {@inheritDoc}
      */
+    @Override
+    public CellType getCellType() {
+        return this.type;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     // Needs to be overridden to display the background image
     public void paintComponent(final Graphics g) {
         super.paintComponent(g);
-        g.drawImage(backgroundImage, 0, 0, this);
+        final Image scaledImage = this.backgroundImage.getScaledInstance(this.getWidth(), this.getHeight(), Image.SCALE_DEFAULT);
+        g.drawImage(scaledImage, 0, 0, this);
     }
 }
