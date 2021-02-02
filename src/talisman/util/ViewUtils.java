fd020009aff3554b043b0a38dd34d5d8f60d2ca6
Alb-Ard <alberto.arduini3@studio.unibo.it>
2021-02-02 11:57:52 +0100

    Creata classe utilità per le constanti della view

---
+++ src/talisman/util/ViewUtils.java
@@ -0,0 +1,17 @@
+package talisman.util;
+
+/**
+ * Utility class for generic view related constants and functions.
+ * 
+ * @author Alberto Arduini
+ *
+ */
+public final class ViewUtils {
+    /**
+     * The path to the "image not found" image file.
+     */
+    public static final String NO_IMAGE_PATH = "imgs/dev/noimage.png";
+
+    private ViewUtils() {
+    }
+}
