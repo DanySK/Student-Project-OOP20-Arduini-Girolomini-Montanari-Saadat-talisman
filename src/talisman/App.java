6ecaf4ffcf64b00ab4e51cc64e6ed4ac6c86cd7d
Alb-Ard <alberto.arduini3@studio.unibo.it>
2021-02-05 18:11:36 +0100

    Creata factory per il tabellone del gioco

--- src/talisman/App.java
+++ src/talisman/App.java
@@ -1,12 +1,10 @@
 package talisman;
 
-import java.awt.Component;
-import java.awt.GridLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
 import java.awt.LayoutManager;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -15,54 +13,42 @@ import javax.swing.WindowConstants;
 import talisman.controller.board.TalismanBoardController;
 
 import talisman.model.board.TalismanBoard;
-import talisman.model.board.TalismanBoardCell;
+import talisman.model.board.TalismanBoardFactory;
 import talisman.model.board.TalismanBoardPawn;
-import talisman.model.board.TalismanBoardSection;
-import talisman.model.board.TalismanCellType;
 import talisman.view.board.PopulatedBoardView;
 import talisman.view.board.PopulatedBoardViewBuilder;
-import talisman.util.CellType;
 
 public final class App {
+    private static final int START_WINDOW_SIZE_X = 1280;
+    private static final int START_WINDOW_SIZE_Y = 720;
+
     private App() {
     }
 
     private void run() {
         // Hard-coded swing GUI for testing
         final JFrame window = new JFrame();
-        final LayoutManager layout = new GridLayout(1, 1);
+        final LayoutManager layout = new GridBagLayout();
         window.setLayout(layout);
-        final PopulatedBoardView boardView = createBoard().getView();
-        window.getContentPane().add((JPanel) boardView);
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        window.setSize(600, 600);
+        window.setSize(START_WINDOW_SIZE_X, START_WINDOW_SIZE_Y);
+        final TalismanBoardController boardController = this.createBoard();
+        final PopulatedBoardView boardView = boardController.getView();
+        final JPanel swingBoardView = (JPanel) boardView;
+        final int edgeSize = Integer.min(START_WINDOW_SIZE_X, START_WINDOW_SIZE_Y);
+        swingBoardView.setSize(edgeSize, edgeSize);
+        final GridBagConstraints constraint = new GridBagConstraints();
+        constraint.gridx = 0;
+        constraint.gridy = 0;
+        constraint.fill = GridBagConstraints.NONE;
+        window.getContentPane().add(swingBoardView, constraint);
         window.setVisible(true);
-        
-        System.out.println(((Component) boardView.getSection(0).getCell(0)).getSize());
     }
 
     private TalismanBoardController createBoard() {
-        // For now all hard-coded for testing
-        // Create model
-        final List<TalismanBoardSection> sections = new ArrayList<>();
-        sections.add(TalismanBoardSection.createSection(
-                List.of(TalismanBoardCell.createCell("", "Test Up", CellType.UP, TalismanCellType.BIOME, Set.of()),
-                        TalismanBoardCell.createCell("", "Test Up", CellType.UP, TalismanCellType.BIOME, Set.of()),
-                        TalismanBoardCell.createCell("", "Test Left", CellType.LEFT, TalismanCellType.BIOME, Set.of()),
-                        TalismanBoardCell.createCell("", "Test Left", CellType.LEFT, TalismanCellType.BIOME, Set.of()),
-                        TalismanBoardCell.createCell("", "Test Down", CellType.DOWN, TalismanCellType.BIOME, Set.of()),
-                        TalismanBoardCell.createCell("", "Test Down", CellType.DOWN, TalismanCellType.BIOME, Set.of()),
-                        TalismanBoardCell.createCell("", "Test Right", CellType.RIGHT, TalismanCellType.BIOME, Set.of()),
-                        TalismanBoardCell.createCell("", "Test Right", CellType.RIGHT, TalismanCellType.BIOME, Set.of()))));
-        sections.add(TalismanBoardSection.createSection(
-                List.of(TalismanBoardCell.createCell("", "Test Up", CellType.UP, TalismanCellType.BIOME, Set.of()),
-                        TalismanBoardCell.createCell("", "Test Left", CellType.LEFT, TalismanCellType.BIOME, Set.of()),
-                        TalismanBoardCell.createCell("", "Test Down", CellType.DOWN, TalismanCellType.BIOME, Set.of()),
-                        TalismanBoardCell.createCell("", "Test Right", CellType.RIGHT, TalismanCellType.BIOME, Set.of()))));
-        final List<TalismanBoardPawn> pawns = new ArrayList<>();
-        pawns.add(TalismanBoardPawn.createPawn(""));
-        final TalismanBoard board = TalismanBoard.createBoard(sections, pawns);
-        // Create view based on model
+        // TODO: change to models created from the menu
+        final List<TalismanBoardPawn> pawns = List.of(TalismanBoardPawn.createPawn(""));
+        final TalismanBoard board = TalismanBoardFactory.createDefaultBoardModel(pawns);
         final PopulatedBoardViewBuilder viewBuilder = new PopulatedBoardViewBuilder();
         return TalismanBoardController.create(board, viewBuilder.buildFromModel(board));
     }
