786d3d2df4ad3a4b1ed53f8db9e6fb3c0cbba5b6
Alb-Ard <alberto.arduini3@studio.unibo.it>
2021-02-05 11:49:04 +0100

    Astratta logica sfondo della cella ereditando ImagePanel

--- src/talisman/view/board/BoardCellViewImpl.java
+++ src/talisman/view/board/BoardCellViewImpl.java
@@ -3,18 +3,13 @@ package talisman.view.board;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Image;
 import java.awt.LayoutManager;
-import java.io.File;
-import java.io.IOException;
 
-import javax.imageio.ImageIO;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import talisman.util.CellType;
-import talisman.util.ViewUtils;
+import talisman.view.ImagePanel;
 
 /**
  * An implementation for a swing-based cell view.
@@ -23,10 +18,9 @@ import talisman.util.ViewUtils;
  *
  */
 // getX and getY do not get overridden since they are already define in JComponent
-public final class BoardCellViewImpl extends JPanel implements BoardCellView {
+public final class BoardCellViewImpl extends ImagePanel implements BoardCellView {
     private final JLabel textLabel;
     private final JPanel textBackground;
-    private final Image backgroundImage;
     private final CellType type;
 
     /**
@@ -37,23 +31,10 @@ public final class BoardCellViewImpl extends JPanel implements BoardCellView {
      * @param type      the type of the cell
      */
     public BoardCellViewImpl(final String imagePath, final String text, final CellType type) {
+        super(imagePath);
         this.type = type;
-        // I try to load the specified image
-        File imageFile = new File(imagePath);
-        // If the image doesn't exist, then I default to the "image not found" image
-        if (!imageFile.exists() || !imageFile.isFile()) {
-            imageFile = new File(ViewUtils.PROJ_NO_IMAGE_PATH);
-        }
-        Image loadedImage = null;
-        try {
-            loadedImage = ImageIO.read(imageFile);
-        } catch (IOException e) {
-            // Shouldn't happen, since the file in "*_NO_IMAGE_PATH" should always exist
-            e.printStackTrace();
-        }
         final LayoutManager layout = new BorderLayout();
         this.setLayout(layout);
-        this.backgroundImage = loadedImage;
         this.textBackground = new JPanel();
         this.textBackground.setBackground(Color.BLACK);
         this.add(this.textBackground, BorderLayout.PAGE_END);
@@ -70,16 +51,5 @@ public final class BoardCellViewImpl extends JPanel implements BoardCellView {
     @Override
     public CellType getCellType() {
         return this.type;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    // Needs to be overridden to display the background image
-    public void paintComponent(final Graphics g) {
-        super.paintComponent(g);
-        final Image scaledImage = this.backgroundImage.getScaledInstance(this.getWidth(), this.getHeight(), Image.SCALE_DEFAULT);
-        g.drawImage(scaledImage, 0, 0, this);
     }
 }
