c2100bdbdedf54348de7ff5a0464981341847aab
Alice Girolomini <alice.girolomini@studio.unibo.it>
2021-02-08 16:09:06 +0100

    Creazione interfaccia controller battaglia

--- src/talisman/model/battle/PlayerInfos.java
+++ src/talisman/model/battle/PlayerInfos.java
@@ -3,7 +3,7 @@ package talisman.model.battle;
 import java.util.List;
 
 /**
- * Implementation  .
+ * A class that helps accessing player's informations.
  * 
  * @author Alice Girolomini
  *
@@ -12,13 +12,33 @@ public final class PlayerInfos {
 
     private static List<PlayerModel> players;
 
+    /**
+     * Creates player's informations.
+     * 
+     * @param players - list of active players
+     */
     private PlayerInfos(final List<PlayerModel> players) {
         PlayerInfos.players = players;
     }
+
+    /**
+     * Gets the player at the specified index.
+     * 
+     * @param index - index of the player
+     * 
+     * @return the player
+     */
     static PlayerModel getPlayer(final int index) {
         return PlayerInfos.players.get(index);
     }
 
+    /**
+     * Adds the player to the player's list.
+     * 
+     * @param player - the player to be add
+     * 
+     * @return the index of the new player
+     */
     static int addPlayer(final PlayerModel player) {
         PlayerInfos.players.add(player);
         return PlayerInfos.players.indexOf(player);
