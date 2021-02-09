9f233fefefbbb02f7db5ddf1bad3cb71f263a709
Alice Girolomini <alice.girolomini@studio.unibo.it>
2021-02-09 13:25:14 +0100

    Modifiche interfaccia controller battaglia

--- src/talisman/controller/battle/BattleController.java
+++ src/talisman/controller/battle/BattleController.java
@@ -1,15 +1,20 @@
 package talisman.controller.battle;
 
+import talisman.model.battle.BattleModel;
+
 /**
  * A MVC controller for the battle.
  * 
  * @author Alice Girolomini
  */
 public interface BattleController {
+    
+    BattleModel getBattle();
+    
+    void updateFate();
 
-    int updateRoll();
+    int updateRoll(int i);
 
-    int udpateScore();
+    int udpateScore(int i);
 
-    void end();
 }
