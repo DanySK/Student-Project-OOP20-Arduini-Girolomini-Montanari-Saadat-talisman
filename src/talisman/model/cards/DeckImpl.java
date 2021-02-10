7456579218e578e6fc294084f2467378267a9a78
Abtin Saadat <abtinsaadat@gmail.com>
2021-02-10 20:42:03 +0100

    Aggiunti i metodi per creare un Deck (da finire)

--- src/talisman/model/cards/DeckImpl.java
+++ src/talisman/model/cards/DeckImpl.java
@@ -1,5 +1,23 @@
 package talisman.model.cards;
 
-public abstract class DeckImpl implements Deck {
+import java.util.List;
 
+public class DeckImpl implements Deck {
+    private List<Card> cards;
+
+    public DeckImpl() {
+        cards = TalismanDeckFactory.createDeck();
+    }
+    public List<Card> getCards() {
+        return cards;
+    }
+    @Override
+    public Card draw() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    @Override
+    public int getNumberOfCards() {
+        return cards.size();
+    }
 }
--- src/talisman/model/cards/TalismanDeckFactory.java
+++ src/talisman/model/cards/TalismanDeckFactory.java
@@ -1,4 +1,8 @@
 package talisman.model.cards;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * A class that generates that generates the needed decks.
  * 
@@ -6,5 +10,12 @@ package talisman.model.cards;
  *
  */
 public class TalismanDeckFactory {
-
+    public static List<Card> createDeck(){
+        final List<Card> cards = new ArrayList<>();
+        //cards.addAll();
+        return null;
+    }
+    public static Card createCard(final String name, final String text, final String imagepath, final CardType type) {
+        return null;
+    }
 }
