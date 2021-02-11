a5d07392f0ee783c1addd5144be8d345e90a0046
Abtin Saadat <abtinsaadat@gmail.com>
2021-02-11 18:57:55 +0100

    Finito implementazione CardImpl (da sistemare il campo actions)

--- src/talisman/model/cards/CardImpl.java
+++ src/talisman/model/cards/CardImpl.java
@@ -1,9 +1,27 @@
 package talisman.model.cards;
 
-public abstract class CardImpl implements Card {
-    private String text;
-    private CardType type;
+import java.util.Collection;
 
+import talisman.model.board.action.TalismanCellAction;
+
+public class CardImpl implements Card {
+    private final String name;
+    private final String text;
+    private final String imagepath;
+    private final CardType type;
+    private final Collection<TalismanCellAction> actions;
+
+    public CardImpl(final String name, final String text, final String imagepath, final CardType type, final Collection<TalismanCellAction> actions) {
+        this.name = name;
+        this.text = text;
+        this.imagepath = imagepath;
+        this.type = type;
+        this.actions = actions;
+    }
+
+    public String getName() {
+        return name;
+    }
     @Override
     public String getText() {
         return text;
@@ -14,4 +32,14 @@ public abstract class CardImpl implements Card {
         return type;
     }
 
+    public Collection<TalismanCellAction> getActions() {
+        return actions;
+    }
+
+    public String getImagePath() {
+        return imagepath;
+    }
+    public static Card createCard(final String name, final String text, final String imagepath, final CardType type, final Collection<TalismanCellAction> actions) {
+        return new CardImpl(name, text, imagepath, type, actions);
+    }
 }
