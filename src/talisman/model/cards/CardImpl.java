921472f3003403aad1db0ebc9458bdb062bb7f37
Abtin Saadat <abtinsaadat@gmail.com>
2021-02-11 19:36:38 +0100

    Sistemato CardImpl

--- src/talisman/model/cards/CardImpl.java
+++ src/talisman/model/cards/CardImpl.java
@@ -2,16 +2,16 @@ package talisman.model.cards;
 
 import java.util.Collection;
 
-import talisman.model.board.action.TalismanCellAction;
+import talisman.model.action.TalismanAction;
 
 public class CardImpl implements Card {
     private final String name;
     private final String text;
     private final String imagepath;
     private final CardType type;
-    private final Collection<TalismanCellAction> actions;
+    private final Collection<TalismanAction> actions;
 
-    public CardImpl(final String name, final String text, final String imagepath, final CardType type, final Collection<TalismanCellAction> actions) {
+    public CardImpl(final String name, final String text, final String imagepath, final CardType type, final Collection<TalismanAction> actions) {
         this.name = name;
         this.text = text;
         this.imagepath = imagepath;
@@ -32,14 +32,14 @@ public class CardImpl implements Card {
         return type;
     }
 
-    public Collection<TalismanCellAction> getActions() {
+    public Collection<TalismanAction> getActions() {
         return actions;
     }
 
     public String getImagePath() {
         return imagepath;
     }
-    public static Card createCard(final String name, final String text, final String imagepath, final CardType type, final Collection<TalismanCellAction> actions) {
+    public static Card createCard(final String name, final String text, final String imagepath, final CardType type, final Collection<TalismanAction> actions) {
         return new CardImpl(name, text, imagepath, type, actions);
     }
 }
