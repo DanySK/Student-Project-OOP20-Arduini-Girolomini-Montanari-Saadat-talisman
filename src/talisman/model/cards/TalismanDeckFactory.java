cd07ae956802de42adb8722e6e05e403008b0df5
Abtin Saadat <abtinsaadat@gmail.com>
2021-02-12 18:39:44 +0100

    Separato la creazione di deck nel factory per i vari tipi di deck

--- src/talisman/model/cards/TalismanDeckFactory.java
+++ src/talisman/model/cards/TalismanDeckFactory.java
@@ -1,7 +1,10 @@
 package talisman.model.cards;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+
+import talisman.model.action.*;
 
 /**
  * A class that generates that generates the needed decks.
@@ -9,13 +12,41 @@ import java.util.List;
  * @author Abtin Saadat
  *
  */
-public class TalismanDeckFactory {
-    public static List<Card> createDeck(){
-        final List<Card> cards = new ArrayList<>();
-        //cards.addAll();
-        return null;
+public final class TalismanDeckFactory {
+    private TalismanDeckFactory() {
     }
-    public static Card createCard(final String name, final String text, final String imagepath, final CardType type) {
-        return null;
+    public static List<Card> createDeck(final DeckType type) {
+        final List<Card> cards = new ArrayList<>();
+        switch (type) {
+        case ADVENTURE:
+            return createAdventureDeck(cards);
+        case SPELL:
+            return createSpellDeck(cards);
+        case SHOP:
+            return createShopDeck(cards);
+        case TALISMAN:
+            return createTalismanDeck(cards);
+        default:
+            break;
+        }
+        cards.add(TalismanDeckFactory.createCard("bag of gold", "get 1 gold", "", CardType.OBJECT, List.of(new TalismanModifyStatisticAction(-1, TalismanActionStatistic.GOLD))));
+        return cards;
+    }
+    private static List<Card> createSpellDeck(final List<Card> cards) {
+        return cards;
+    }
+    private static List<Card> createAdventureDeck(final List<Card> cards) {
+        return cards;
+    }
+    private static List<Card> createShopDeck(final List<Card> cards) {
+        return cards;
+    }
+    private static List<Card> createTalismanDeck(final List<Card> cards) {
+        return cards;
+    }
+
+    public static Card createCard(final String name, final String text, final String imagepath, final CardType type,
+            final Collection<TalismanAction> actions) {
+        return CardImpl.createCard(name, text, imagepath, type, actions);
     }
 }
