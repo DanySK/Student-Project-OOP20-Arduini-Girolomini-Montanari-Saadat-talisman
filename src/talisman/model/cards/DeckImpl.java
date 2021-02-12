2f404e421acf1a42ce8c09d35841ce0e5120bba0
Abtin Saadat <abtinsaadat@gmail.com>
2021-02-12 19:10:42 +0100

    Sistemato Deck e implementazione DeckImpl

--- src/talisman/model/cards/DeckImpl.java
+++ src/talisman/model/cards/DeckImpl.java
@@ -1,23 +1,42 @@
 package talisman.model.cards;
 
 import java.util.List;
+import java.util.Queue;
 
 public class DeckImpl implements Deck {
-    private List<Card> cards;
+    private final List<Card> cards;
+    private final DeckType type;
 
-    public DeckImpl() {
-        cards = TalismanDeckFactory.createDeck();
+    public DeckImpl(final DeckType type, final List<Card> cards) {
+        this.type = type;
+        this.cards = List.copyOf(cards);
     }
+    /**
+     * {@inheritDoc}
+     */
     public List<Card> getCards() {
-        return cards;
+        return this.cards;
     }
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Card draw() {
         // TODO Auto-generated method stub
         return null;
     }
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int getNumberOfCards() {
         return cards.size();
+    }
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public DeckType getType() {
+        return this.type;
     }
 }
