138c8adcb69a9020e654fc02b90e2d3fb6a7a767
Alb-Ard <alberto.arduini3@studio.unibo.it>
2021-02-14 16:42:56 +0100

    Aggiunto riferimento statico al controller per i giocatori alla classe Controllers

--- src/talisman/Controllers.java
+++ src/talisman/Controllers.java
@@ -1,17 +1,48 @@
 package talisman;
 
 import talisman.controller.board.TalismanBoardController;
+import talisman.controller.character.CharactersController;
 
+/**
+ * Static utility class that holds references to all the controllers that should
+ * be accessed statically. It also prevents multiple controller instances
+ * assignments during a game.
+ * 
+ * @author Alberto Arduini
+ *
+ */
 public final class Controllers {
     private static TalismanBoardController boardController;
+    private static CharactersController charactersController;
 
     private Controllers() {
     }
 
+    /**
+     * Gets the active board controller.
+     * 
+     * @return the controller
+     */
     public static TalismanBoardController getBoardController() {
         return Controllers.boardController;
     }
 
+    /**
+     * Gets the active characters controller.
+     * 
+     * @return the controller
+     */
+    public static CharactersController getCharactersController() {
+        return Controllers.charactersController;
+    }
+
+    /**
+     * Sets the active board controller.
+     * 
+     * @param controller the controller to set
+     * 
+     * @throws IllegalStateException if the controller is already assigned
+     */
     public static void setBoardController(final TalismanBoardController controller) {
         if (Controllers.boardController != null) {
             throw new IllegalStateException("The controller was already assigned");
@@ -19,7 +50,25 @@ public final class Controllers {
         Controllers.boardController = controller;
     }
 
+    /**
+     * Sets the active character controller.
+     * 
+     * @param controller the controller to set
+     * 
+     * @throws IllegalStateException if the controller is already assigned
+     */
+    public static void setCharactersController(final CharactersController controller) {
+        if (Controllers.charactersController != null) {
+            throw new IllegalStateException("The controller was already assigned");
+        }
+        Controllers.charactersController = controller;
+    }
+
+    /**
+     * Resets the controller references. This should be called when ending a game.
+     */
     public static void reset() {
         Controllers.boardController = null;
+        Controllers.charactersController = null;
     }
 }
