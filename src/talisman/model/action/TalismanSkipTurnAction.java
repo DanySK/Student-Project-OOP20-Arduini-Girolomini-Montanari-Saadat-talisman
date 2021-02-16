4d18cbf8f8d2a3b689a7ae90cd839ada6cd2b093
Alb-Ard <alberto.arduini3@studio.unibo.it>
2021-02-16 18:54:45 +0100

    Aggiunta azione che fa saltare il turno al giocatore

---
+++ src/talisman/model/action/TalismanSkipTurnAction.java
@@ -0,0 +1,30 @@
+package talisman.model.action;
+
+/**
+ * Models an action that skips to the next player turn.
+ * 
+ * @author Alberto Arduini
+ *
+ */
+public class TalismanSkipTurnAction implements TalismanAction {
+    /**
+     * Gets what to show if no action is present.
+     */
+    private static final String DESCRIPTION = "Skip your turn";
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getDescription() {
+        return TalismanSkipTurnAction.DESCRIPTION;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void apply() {
+        // TODO: Skip turn
+    }
+}
