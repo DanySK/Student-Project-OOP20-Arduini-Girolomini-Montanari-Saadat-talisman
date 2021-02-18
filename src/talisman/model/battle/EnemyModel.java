f04573cbe82f0ea953efd24f09ff894163b3dea9
Alice Girolomini <alice.girolomini@studio.unibo.it>
2021-02-18 15:12:34 +0100

    Creazione interfaccia nemico

---
+++ src/talisman/model/battle/EnemyModel.java
@@ -0,0 +1,13 @@
+package talisman.model.battle;
+
+import talisman.model.character.CharacterModel;
+
+/**
+ * Interface of enemy's informations in battle.
+ * 
+ * @author Alice Girolomini
+ *
+ */
+public interface EnemyModel extends CharacterModel {
+
+}
