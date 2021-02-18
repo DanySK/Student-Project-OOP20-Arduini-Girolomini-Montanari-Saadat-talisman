16b588f7d451c18fcd97e9cbbbee92afd092532e
Abtin Saadat <abtinsaadat@gmail.com>
2021-02-19 00:06:04 +0100

    Aggiunto la parte per testare la view

--- src/talisman/App.java
+++ src/talisman/App.java
@@ -3,17 +3,26 @@ package talisman;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.LayoutManager;
+import java.util.List;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.WindowConstants;
 
+import talisman.controller.cards.TalismanCardController;
+import talisman.model.action.TalismanActionStatistic;
+import talisman.model.action.TalismanModifyStatisticAction;
+import talisman.model.cards.Card;
+import talisman.model.cards.CardImpl;
+import talisman.model.cards.CardType;
+import talisman.model.cards.DeckType;
+import talisman.model.cards.TalismanDeckFactory;
 import talisman.util.GameSetupUtil;
 
 import talisman.view.battle.BattleBottomViewImpl;
 import talisman.view.battle.BattleCenterViewImpl;
 import talisman.view.battle.BattleTopViewImpl;
-
+import talisman.view.cards.TalismanCardView;
 import talisman.view.menu.MainMenuWindow;
 
 public final class App {
@@ -26,7 +35,20 @@ public final class App {
 
     private void run() {
         this.showMainMenu();
-
+        // test card view
+        final JFrame thirdWindow = new JFrame();
+        final LayoutManager thirdlayout = new GridBagLayout();
+        final GridBagConstraints thirdconstraint = new GridBagConstraints();
+        thirdconstraint.gridx = 0;
+        thirdconstraint.gridy = 0;
+        thirdconstraint.fill = GridBagConstraints.BOTH;
+        thirdWindow.setLayout(thirdlayout);
+        thirdWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        final JPanel card = (JPanel) TalismanCardController.createView(TalismanDeckFactory.createDeck(DeckType.ADVENTURE).draw());
+        thirdWindow.getContentPane().add(card, thirdconstraint);
+        thirdWindow.pack();
+        thirdWindow.setResizable(false);
+        thirdWindow.setVisible(true);
         // test battle view
         final JFrame secondWindow = new JFrame();
         final LayoutManager secondLayout = new GridBagLayout();
@@ -37,18 +59,20 @@ public final class App {
         secondWindow.setLayout(secondLayout);
         secondWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         final JPanel swingBattleTopView = new BattleTopViewImpl();
-        final JPanel swingBattleCenterView = new BattleCenterViewImpl();
+        //final JPanel swingBattleCenterView = new BattleCenterViewImpl();
         final JPanel swingBattleBottomView = new BattleBottomViewImpl();
         secondWindow.getContentPane().add(swingBattleTopView, secondConstraint);
         secondConstraint.gridx = 0;
         secondConstraint.gridy = 1;
-        secondWindow.getContentPane().add(swingBattleCenterView, secondConstraint);
+        //secondWindow.getContentPane().add(swingBattleCenterView, secondConstraint);
         secondConstraint.gridx = 0;
         secondConstraint.gridy = 2;
         secondWindow.getContentPane().add(swingBattleBottomView, secondConstraint);
         secondWindow.pack();
         secondWindow.setResizable(false);
         secondWindow.setVisible(true);
+
+
     }
 
     private void showMainMenu() {
