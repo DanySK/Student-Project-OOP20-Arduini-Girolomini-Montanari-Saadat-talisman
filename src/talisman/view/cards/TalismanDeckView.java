451381a60f2b850e3cb45dec7f497b616e662cb4
Abtin Saadat <abtinsaadat@gmail.com>
2021-02-21 23:29:36 +0100

    Testing su TalismanDeckView e CardView per visualizzare lista di carte

--- src/talisman/view/cards/TalismanDeckView.java
+++ src/talisman/view/cards/TalismanDeckView.java
@@ -1,9 +1,36 @@
 package talisman.view.cards;
 
-import talisman.model.cards.Deck;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+
+import javax.swing.JButton;
+
+import talisman.model.cards.Card;
 
 public interface TalismanDeckView {
-    static TalismanCardView create(final Deck deck) {
-        return new TalismanDeckViewImpl.createView(deck);
+    static void create(final List<Card> deck) {
+        int index = 0;
+        TalismanCardViewImpl deckView = new TalismanCardViewImpl(deck.get(index).getImagePath(), deck.get(index).getText());
+        JButton button = new JButton("Click 0");
+        button.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                index = index -1;
+                deckView.setView(deck.get(index).getImagePath(), deck.get(index).getText());
+            }
+        });
+        JButton button2 = new JButton("Click 1");
+        button2.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                // TODO Auto-generated method stub
+                deckView.setView(deck.get(index).getImagePath(), deck.get(index).getText());
+            }
+        });
+        deckView.add(button);
+        deckView.add(button2);
     }
 }
