687e2e7b856cd71f73cada71eb4d7f5b148c24ef
Alice Girolomini <alice.girolomini@studio.unibo.it>
2021-02-22 16:42:20 +0100

    Correzioni controller morti e aggiunti commenti

--- src/talisman/controller/battle/DeathController.java
+++ src/talisman/controller/battle/DeathController.java
@@ -1,11 +1,32 @@
 package talisman.controller.battle;
+
+import talisman.model.character.CharacterModel;
+
 /**
  * A MVC controller for the character's death.
  * 
  * @author Alice Girolomini
  */
 public interface DeathController {
-
+    /**
+     * Manages the character's death.
+     * 
+     * @return true if the character died
+     */
     boolean death();
+
+    /**
+     * Resets the character's informations.
+     * 
+     * @param character - the character to reset
+     */
+    void resetCharacterInfo(CharacterModel character);
+
+    /**
+     * Resets the character's position on the board.
+     * 
+     * @param index - the index of the player
+     */
+    void resetCharacterPosition(int index);
 
 }
