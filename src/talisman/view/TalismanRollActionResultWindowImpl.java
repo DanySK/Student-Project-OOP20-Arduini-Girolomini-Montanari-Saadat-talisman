37aa8df3b4c873fe6916231e209ae94188688b70
Alb-Ard <alberto.arduini3@studio.unibo.it>
2021-02-23 22:00:12 +0100

    Sistemato popup dopo tiro del dato da TalismanRollAction

--- src/talisman/view/TalismanRollActionResultWindowImpl.java
+++ src/talisman/view/TalismanRollActionResultWindowImpl.java
@@ -3,12 +3,10 @@ package talisman.view;
 import javax.swing.JOptionPane;
 
 public class TalismanRollActionResultWindowImpl implements TalismanRollActionResultWindow {
-    private static final String TITLE_FORMAT = "Roll result";
     private static final String LABEL_FORMAT = "The result is %d:" + System.lineSeparator() + "%s ";
 
     public TalismanRollActionResultWindowImpl(final int result, final String actionDescription) {
-        JOptionPane.showConfirmDialog(null,
-                String.format(TalismanRollActionResultWindowImpl.LABEL_FORMAT, result, actionDescription),
-                TalismanRollActionResultWindowImpl.TITLE_FORMAT, JOptionPane.OK_OPTION);
+        JOptionPane.showMessageDialog(null,
+                String.format(TalismanRollActionResultWindowImpl.LABEL_FORMAT, result, actionDescription));
     }
 }
