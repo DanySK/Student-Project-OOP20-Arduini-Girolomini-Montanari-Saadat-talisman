8baeacd711594b81ed465c97efbd2202f2936a11
Abtin Saadat <abtinsaadat@gmail.com>
2021-02-23 12:57:56 +0100

    Aggiunti javadoc

--- src/talisman/view/cards/TalismanCardView.java
+++ src/talisman/view/cards/TalismanCardView.java
@@ -1,6 +1,16 @@
 package talisman.view.cards;
-
+/**
+ * Creates the view for a single card.
+ * @author Abtin Saadat
+ *
+ */
 public interface TalismanCardView {
+    /**
+     * Creates the view for a single card.
+     * @param imagePath The card's image path
+     * @param text The card's text
+     * @return Returns the view 
+     */
     static TalismanCardView create(final String imagePath, final String text) {
         return new TalismanCardViewImpl(imagePath, text);
     }
