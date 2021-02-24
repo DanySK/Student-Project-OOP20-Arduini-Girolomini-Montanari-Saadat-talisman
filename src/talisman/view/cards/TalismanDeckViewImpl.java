f5c76194adb655173a4dd6366b7b22d89e251a2c
Abtin Saadat <abtinsaadat@gmail.com>
2021-02-24 19:04:56 +0100

    Refactoring View del deck

--- src/talisman/view/cards/TalismanDeckViewImpl.java
+++ src/talisman/view/cards/TalismanDeckViewImpl.java
@@ -1,46 +1,63 @@
 package talisman.view.cards;
 
-import java.awt.Color;
+import java.awt.LayoutManager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JPanel;
 
 import talisman.model.cards.Card;
 
-public class TalismanDeckViewImpl implements TalismanDeckView {
+public class TalismanDeckViewImpl extends JPanel implements TalismanDeckView {
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    private static final int MAX_SHOWN_CARDS = 2;
     private int index;
-    private final TalismanCardViewImpl deckView;
+    private final List<TalismanCardView> cards;
+    private final JPanel cardsPanel;
 
     public TalismanDeckViewImpl(final List<Card> deck) {
-        index = 0;
-        deckView = new TalismanCardViewImpl(deck.get(index).getImagePath(), deck.get(index).getText(),
-                deck.get(index).getName());
+        this.index = 0;
+        final LayoutManager layout = new BoxLayout(this, BoxLayout.X_AXIS);
+        this.setLayout(layout);
         final JButton button = new JButton("<<");
         button.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(final ActionEvent e) {
-                index = index - 1;
-                deckView.setView(deck.get(index).getImagePath(), deck.get(index).getText(), deck.get(index).getName());
+                updateCards(index - 1);
             }
         });
+        this.add(button);
+        cards = new ArrayList<>();
+        for (final Card card : deck) {
+            cards.add(new TalismanCardViewImpl(deck.get(index).getImagePath(), deck.get(index).getText(),
+                    deck.get(index).getName()));
+        }
+        this.cardsPanel = new JPanel();
+        this.add(this.cardsPanel);
         final JButton button2 = new JButton(">>");
         button2.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(final ActionEvent e) {
-                index = index + 1;
-                deckView.setView(deck.get(index).getImagePath(), deck.get(index).getText(), deck.get(index).getName());
+                updateCards(index + 1);
             }
         });
-        deckView.add(button);
-        deckView.add(button2);
+        this.add(button2);
     }
 
-    public final TalismanCardView createDeckView() {
-        return this.deckView;
+    private void updateCards(final int index) {
+        this.index = Math.min(Math.max(index, 0), this.cards.size() - MAX_SHOWN_CARDS);
+        this.cardsPanel.removeAll();
+        for (int i = this.index; i < this.index + MAX_SHOWN_CARDS; i++) {
+            this.cardsPanel.add((TalismanCardViewImpl) this.cards.get(i));
+        }
     }
 }
