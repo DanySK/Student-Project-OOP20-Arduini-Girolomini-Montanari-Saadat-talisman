8263ad5a28dffe351832cf6da24872456562d35e
Abtin Saadat <abtinsaadat@gmail.com>
2021-02-25 22:16:05 +0100

    Aggiornato javadoc

--- src/talisman/view/cards/TalismanDeckView.java
+++ src/talisman/view/cards/TalismanDeckView.java
@@ -3,7 +3,11 @@ package talisman.view.cards;
 import java.util.List;
 
 import talisman.model.cards.Card;
-
+/**
+ * A class with a method that creates a view of a list of cards.
+ * @author Abtin Saadat
+ *
+ */
 public interface TalismanDeckView {
     static TalismanDeckView create(final List<Card> deck) {
         return new TalismanDeckViewImpl(deck);
